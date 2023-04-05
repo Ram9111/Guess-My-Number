d5df5fa194114137e968252ef5f152ce59176d2e
Ram choudhary <ramchoudhary9111@gmail.com>
2023-04-05 10:32:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+/*
+console.log(document.querySelector(`.message`).textContent); //with the help og query selectar we fstch the
+//html class whose name is .message
+document.querySelector('.message').textContent = '🎉currect number'; //we change the text content start guessing to current number
+
+document.querySelector('.number').textContent = 13; // here we give the value of 13 in class .number
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);*/
+
+/*const x = function () {
+  console.log(23);
+};
+document.querySelector('.check').addEventListener('click', function () {
+  //check class here
+  console.log(document.querySelector('.guess').value);
+
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    //notequal to guess than exicute
+    document.querySelector('.message'), (testcontent = 'no number!'); // pass .message class ke ander print hoga ager input no. wrong hoga to "no number"
+  }
+});*/
